66b6d90ccd496825feab8df59abe8cf712aa9960
mmosquera <mmosquera@makingsense.com>
2018-11-20 10:34:18 -0300

    add attachments and init collection

--- Relay.BulkSenderService/Classes/ApiRecipient.cs
+++ Relay.BulkSenderService/Classes/ApiRecipient.cs
@@ -4,56 +4,59 @@ using System.IO;
 
 namespace Relay.BulkSenderService.Classes
 {
-    public class ApiRecipient : Recipient
-    {
-        public string TemplateId { get; set; }
-        public Dictionary<string, object> Fields { get; set; }
-        public string Key { get; set; }
+	public class ApiRecipient : Recipient
+	{
+		public string TemplateId { get; set; }
+		public Dictionary<string, object> Fields { get; set; }
+		public string Key { get; set; }
 
-        public ApiRecipient()
-        {
-            HasError = false;
-            ToEmail = null;
-            ToName = null;
-            FromEmail = null;
-            FromName = null;
-            ReplyToEmail = null;
-            ReplyToName = null;
-            Fields = new Dictionary<string, object>();
-        }
+		public ApiRecipient()
+		{
+			HasError = false;
+			ToEmail = null;
+			ToName = null;
+			FromEmail = null;
+			FromName = null;
+			ReplyToEmail = null;
+			ReplyToName = null;
+			Fields = new Dictionary<string, object>();
+		}
 
-        public void FillAttachments(List<string> files)
-        {
-            Attachments = new List<RecipientAttachment>();
+		public void FillAttachments(List<string> files)
+		{
+			if (Attachments == null)
+			{
+				Attachments = new List<RecipientAttachment>();
+			}
 
-            foreach (string fileName in files)
-            {
-                byte[] bytesArray = File.ReadAllBytes(fileName);
-                Attachments.Add(new RecipientAttachment()
-                {
-                    Base64String = Convert.ToBase64String(bytesArray),
-                    FileName = Path.GetFileName(fileName),
-                    FileType = GetContentTypeByExtension(fileName),
-                });
-            }
-        }
+			foreach (string fileName in files)
+			{
+				byte[] bytesArray = File.ReadAllBytes(fileName);
+				Attachments.Add(new RecipientAttachment()
+				{
+					Base64String = Convert.ToBase64String(bytesArray),
+					FileName = Path.GetFileName(fileName),
+					FileType = GetContentTypeByExtension(fileName),
+				});
+			}
+		}
 
-        private string GetContentTypeByExtension(string filename)
-        {
-            switch (Path.GetExtension(filename))
-            {
-                case ".zip": return "application/x-zip-compressed";
-                case ".mp3": return "audio/mp3";
-                case ".gif": return "image/gif";
-                case ".jpg": return "image/jpeg";
-                case ".png": return "image/png";
-                case ".htm": return "text/html";
-                case ".html": return "text/html";
-                case ".txt": return "text/plain";
-                case ".xml": return "text/xml";
-                case ".pdf": return "application/pdf";
-                default: return "application/octet-stream";
-            }
-        }
-    }
+		private string GetContentTypeByExtension(string filename)
+		{
+			switch (Path.GetExtension(filename))
+			{
+				case ".zip": return "application/x-zip-compressed";
+				case ".mp3": return "audio/mp3";
+				case ".gif": return "image/gif";
+				case ".jpg": return "image/jpeg";
+				case ".png": return "image/png";
+				case ".htm": return "text/html";
+				case ".html": return "text/html";
+				case ".txt": return "text/plain";
+				case ".xml": return "text/xml";
+				case ".pdf": return "application/pdf";
+				default: return "application/octet-stream";
+			}
+		}
+	}
 }
