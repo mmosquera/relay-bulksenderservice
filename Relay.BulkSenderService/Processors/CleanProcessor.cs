4e2dce988952bfb72a6e0346620dc076af7df9c2
mmosquera <mmosquera@makingsense.com>
2019-05-21 15:23:15 -0300

    fix clean processor

--- Relay.BulkSenderService/Processors/CleanProcessor.cs
+++ Relay.BulkSenderService/Processors/CleanProcessor.cs
@@ -7,113 +7,118 @@ using System.Threading;
 
 namespace Relay.BulkSenderService.Processors
 {
-	public class CleanProcessor : BaseWorker
-	{
-		public CleanProcessor(ILog logger, IConfiguration configuration, IWatcher watcher) : base(logger, configuration, watcher)
-		{
+    public class CleanProcessor : BaseWorker
+    {
+        public CleanProcessor(ILog logger, IConfiguration configuration, IWatcher watcher) : base(logger, configuration, watcher)
+        {
 
-		}
+        }
 
-		public void Process()
-		{
-			while (true)
-			{
-				try
-				{
-					CheckConfigChanges();
+        public void Process()
+        {
+            while (true)
+            {
+                try
+                {
+                    CheckConfigChanges();
 
-					_logger.Debug($"Start to delete local files.");
+                    _logger.Debug($"Start to delete local files.");
 
-					foreach (IUserConfiguration user in _users)
-					{
-						DeleteAttachmentsFiles(user);
+                    foreach (IUserConfiguration user in _users)
+                    {
+                        DeleteAttachmentsFiles(user);
 
-						DeleteLocalFiles(user);
-					}
+                        DeleteLocalFiles(user);
+                    }
 
-					DeleteReportsFiles();
-				}
-				catch (Exception e)
-				{
-					_logger.Error($"General error on clean process -- {e}");
-				}
+                    DeleteReportsFiles();
+                }
+                catch (Exception e)
+                {
+                    _logger.Error($"General error on clean process -- {e}");
+                }
 
-				Thread.Sleep(_configuration.CleanInterval);
-			}
-		}
+                Thread.Sleep(_configuration.CleanInterval);
+            }
+        }
 
-		private void DeleteAttachmentsFiles(IUserConfiguration user)
-		{
-			string attachmentsFolder = new FilePathHelper(_configuration, user.Name).GetAttachmentsFilesFolder();
+        private void DeleteAttachmentsFiles(IUserConfiguration user)
+        {
+            string attachmentsFolder = new FilePathHelper(_configuration, user.Name).GetAttachmentsFilesFolder();
 
-			DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanAttachmentsDays);
+            DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanAttachmentsDays);
 
-			DirectoryInfo directory = new DirectoryInfo(attachmentsFolder);
-			if (directory.Exists)
-			{
-				FileInfo[] files = directory.GetFiles().Where(f => f.CreationTimeUtc < filterDate).ToArray();
-				foreach (FileInfo file in files)
-				{
-					try
-					{
-						file.Delete();
-					}
-					catch (Exception e)
-					{
-						_logger.Error($"Error trying to delete attach file {file.FullName} -- {e}");
-					}
-				}
-			}
-		}
+            DirectoryInfo directory = new DirectoryInfo(attachmentsFolder);
 
-		private void DeleteLocalFiles(IUserConfiguration user)
-		{
-			string path = new FilePathHelper(_configuration, user.Name).GetUserFolder();
+            if (directory.Exists)
+            {
+                DirectoryInfo[] directories = directory.GetDirectories();
 
-			DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanDays);
+                foreach (DirectoryInfo subDirectory in directories)
+                {
+                    DeleteFilesFromFolder(subDirectory, filterDate);
 
-			var baseDirectory = new DirectoryInfo(path);
-			if (baseDirectory.Exists)
-			{
-				DirectoryInfo[] directories = baseDirectory.GetDirectories();
-				foreach (DirectoryInfo directory in directories)
-				{
-					FileInfo[] files = directory.GetFiles().Where(f => f.CreationTimeUtc < filterDate).ToArray();
-					foreach (FileInfo file in files)
-					{
-						try
-						{
-							file.Delete();
-						}
-						catch (Exception e)
-						{
-							_logger.Error($"Error trying to delete file {file.FullName} -- {e}");
-						}
-					}
-				}
-			}
-		}
+                    DeleteFolder(subDirectory, filterDate);
+                }
 
-		private void DeleteReportsFiles()
-		{
-			DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanDays);
+                DeleteFilesFromFolder(directory, filterDate);
+            }
+        }
 
-			var directory = new DirectoryInfo(_configuration.ReportsFolder);
-			if (directory.Exists)
-			{
-				FileInfo[] files = directory.GetFiles().Where(f => f.CreationTimeUtc < filterDate).ToArray();
-				foreach (FileInfo file in files)
-				{
-					try
-					{
-						file.Delete();
-					}
-					catch (Exception e)
-					{
-						_logger.Error($"Error trying to delete file {file.FullName} -- {e}");
-					}
-				}
-			}
-		}
-	}
+        private void DeleteFolder(DirectoryInfo folder, DateTime dateFilter)
+        {
+            if (folder.Exists && folder.GetFiles().Length == 0 && folder.CreationTimeUtc < dateFilter)
+            {
+                try
+                {
+                    folder.Delete();
+                }
+                catch (Exception e)
+                {
+                    _logger.Error($"Error trying to delete folder {folder.FullName} -- {e}");
+                }
+            }
+        }
+
+        private void DeleteFilesFromFolder(DirectoryInfo folder, DateTime dateFilter)
+        {
+            FileInfo[] files = folder.GetFiles().Where(f => f.CreationTimeUtc < dateFilter).ToArray();
+
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception e)
+                {
+                    _logger.Error($"Error trying to delete attach file {file.FullName} -- {e}");
+                }
+            }
+        }
+
+        private void DeleteLocalFiles(IUserConfiguration user)
+        {
+            string path = new FilePathHelper(_configuration, user.Name).GetUserFolder();
+
+            DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanDays);
+
+            var baseDirectory = new DirectoryInfo(path);
+            if (baseDirectory.Exists)
+            {
+                DeleteFilesFromFolder(baseDirectory, filterDate);
+            }
+        }
+
+        private void DeleteReportsFiles()
+        {
+            DateTime filterDate = DateTime.UtcNow.AddDays(-_configuration.CleanDays);
+
+            var directory = new DirectoryInfo(_configuration.ReportsFolder);
+            if (directory.Exists)
+            {
+                DeleteFilesFromFolder(directory, filterDate);
+            }
+        }
+    }
 }
