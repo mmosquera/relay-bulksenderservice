523d966987c8a3d255d0633365dc54a8160cfe65
mmosquera <mmosquera@makingsense.com>
2019-06-06 16:12:58 -0300

    refactor banorte processor

--- Relay.BulkSenderService/Processors/APIProcessorBanorte.cs
+++ Relay.BulkSenderService/Processors/APIProcessorBanorte.cs
@@ -5,45 +5,46 @@ using System.Collections.Generic;
 
 namespace Relay.BulkSenderService.Processors
 {
-	public class APIProcessorBanorte : APIProcessor
-	{
-		public APIProcessorBanorte(ILog logger, IConfiguration configuration) : base(logger, configuration) { }
+    public class APIProcessorBanorte : APIProcessor
+    {
 
-		protected override void FillRecipientAttachments(ApiRecipient recipient, ITemplateConfiguration templateConfiguration, string[] recipientArray, string fileName, string line, UserApiConfiguration user, ProcessResult result)
-		{
-			var attachmentsList = new List<string>();
-			string attachName = null;
-			if (recipientArray.Length >= 4)
-			{
-				attachName = $@"{recipientArray[0]}-{recipientArray[1]}-{recipientArray[2]}-{recipientArray[3]}.pdf";
-			}
+        public APIProcessorBanorte(ILog logger, IConfiguration configuration) : base(logger, configuration) { }
 
-			if (!string.IsNullOrEmpty(attachName))
-			{
-				string localAttachement = GetAttachmentFile(attachName, fileName, user);
+        protected override void FillRecipientAttachments(ApiRecipient recipient, ITemplateConfiguration templateConfiguration, string[] recipientArray, string fileName, string line, UserApiConfiguration user, ProcessResult result)
+        {
+            var attachmentsList = new List<string>();
+            string attachName = null;
+            if (recipientArray.Length >= 4)
+            {
+                attachName = $@"{recipientArray[0]}-{recipientArray[1]}-{recipientArray[2]}-{recipientArray[3]}.pdf";
+            }
 
-				if (!string.IsNullOrEmpty(localAttachement))
-				{
-					attachmentsList.Add(localAttachement);
-				}
-				else
-				{
-					string message = $"The attachment file {attachName} doesn't exists.";
-					recipient.HasError = true;
-					recipient.ResultLine = $"{line}{templateConfiguration.FieldSeparator}{message}";
-					_logger.Error(message);
-					string errorMessage = $"{DateTime.UtcNow}:{message} proccesing line {line}";
-					result.WriteError(errorMessage);
-					result.ErrorsCount++;
-				}
-			}
+            if (!string.IsNullOrEmpty(attachName))
+            {
+                string localAttachement = GetAttachmentFile(attachName, fileName, user);
 
-			if (attachmentsList.Count > 0)
-			{
-				recipient.FillAttachments(attachmentsList);
-			}
+                if (!string.IsNullOrEmpty(localAttachement))
+                {
+                    attachmentsList.Add(localAttachement);
+                }
+                else
+                {
+                    string message = $"The attachment file {attachName} doesn't exists.";
+                    recipient.HasError = true;
+                    recipient.ResultLine = $"{line}{templateConfiguration.FieldSeparator}{message}";
+                    _logger.Error(message);
+                    string errorMessage = $"{DateTime.UtcNow}:{message} proccesing line {line}";
+                    result.WriteError(errorMessage);
+                    result.ErrorsCount++;
+                }
+            }
 
-			base.FillRecipientAttachments(recipient, templateConfiguration, recipientArray, fileName, line, user, result);
-		}
-	}
+            if (attachmentsList.Count > 0)
+            {
+                recipient.FillAttachments(attachmentsList);
+            }
+
+            base.FillRecipientAttachments(recipient, templateConfiguration, recipientArray, fileName, line, user, result);
+        }
+    }
 }
