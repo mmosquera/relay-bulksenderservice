389026bfb4eeb61872d36e28662c325e56ba5a35
mmosquera <mmosquera@makingsense.com>
2019-06-18 12:02:08 -0300

    add hipotecario pre processor

---
+++ Relay.BulkSenderService/Configuration/HipotecarioPreProcessorConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Relay.BulkSenderService.Classes;
+using Relay.BulkSenderService.Processors.PreProcess;
+
+namespace Relay.BulkSenderService.Configuration
+{
+    public class HipotecarioPreProcessorConfiguration : IPreProcessorConfiguration
+    {
+        public PreProcessor GetPreProcessor(ILog logger, IConfiguration configuration)
+        {
+            return new HipotecarioPreProcessor(logger, configuration);
+        }
+    }
+}
---
+++ Relay.BulkSenderService/Processors/PreProcess/HipotecarioPreProcessor.cs
@@ -0,0 +1,76 @@
+﻿using Relay.BulkSenderService.Classes;
+using Relay.BulkSenderService.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Relay.BulkSenderService.Processors.PreProcess
+{
+    public class HipotecarioPreProcessor : PreProcessor
+    {
+        public HipotecarioPreProcessor(ILog logger, IConfiguration configuration) : base(logger, configuration)
+        {
+        }
+
+        public override void ProcessFile(string fileName, string userName)
+        {
+            if (!File.Exists(fileName))
+            {
+                return;
+            }
+
+            if (Path.GetExtension(fileName).Equals(".zip", StringComparison.OrdinalIgnoreCase))
+            {
+                var filePathHelper = new FilePathHelper(_configuration, userName);
+
+                string downloadPath = filePathHelper.GetDownloadsFolder();
+                string processedPath = filePathHelper.GetProcessedFilesFolder();
+
+
+                List<string> zipEntries = new ZipHelper().UnzipFile(fileName, downloadPath);
+
+                try
+                {
+                    File.Delete(fileName); // Delete zip file                        
+                }
+                catch (Exception e)
+                {
+                    _logger.Error($"Error trying to delete zip file -- {e}");
+                }
+
+                foreach (string zipEntry in zipEntries)
+                {
+                    string name = Path.GetFileNameWithoutExtension(zipEntry);
+
+                    if (File.Exists($@"{downloadPath}\{name}.processing") || File.Exists($@"{processedPath}\{name}.processing"))
+                    {
+                        _logger.Info($"The file {zipEntry} is processing.");
+
+                        File.Delete(zipEntry);
+
+                        continue;
+                    }
+
+                    if (File.Exists($@"{processedPath}\{name}.processed"))
+                    {
+                        _logger.Error($"The file {zipEntry} is already processed.");
+
+                        File.Delete(zipEntry);
+
+                        continue;
+                    }
+
+                    string newFileName = zipEntry.Replace(Path.GetExtension(zipEntry), ".processing");
+
+                    File.Move(zipEntry, newFileName);
+                }
+            }
+            else
+            {
+                string newFileName = fileName.Replace(Path.GetExtension(fileName), ".processing");
+
+                File.Move(fileName, newFileName);
+            }
+        }
+    }
+}
