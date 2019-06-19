283676682da73558f1a9ade4f68b2622559146d8
mmosquera <mmosquera@makingsense.com>
2019-06-19 17:16:23 -0300

    add preprocess for Gire

---
+++ Relay.BulkSenderService/Configuration/GirePreProcessorConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Relay.BulkSenderService.Classes;
+using Relay.BulkSenderService.Processors.PreProcess;
+
+namespace Relay.BulkSenderService.Configuration
+{
+    public class GirePreProcessorConfiguration : IPreProcessorConfiguration
+    {
+        public PreProcessor GetPreProcessor(ILog logger, IConfiguration configuration)
+        {
+            return new GirePreProcessor(logger, configuration);
+        }
+    }
+}
