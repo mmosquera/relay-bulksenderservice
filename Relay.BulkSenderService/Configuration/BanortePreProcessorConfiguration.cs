52f99a32ee7eaf7f9e2f6d8055d5cc5042925550
mmosquera <mmosquera@makingsense.com>
2019-11-10 23:24:51 -0300

    add BanortePreProcessorConfiguration

---
+++ Relay.BulkSenderService/Configuration/BanortePreProcessorConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Relay.BulkSenderService.Classes;
+using Relay.BulkSenderService.Processors.PreProcess;
+
+namespace Relay.BulkSenderService.Configuration
+{
+    public class BanortePreProcessorConfiguration : IPreProcessorConfiguration
+    {
+        public PreProcessor GetPreProcessor(ILog logger, IConfiguration configuration)
+        {
+            return new BanortePreProcessor(logger, configuration);
+        }
+    }
+}
