25448386511a76e4bb6ae6f878f1f3c14378c522
mmosquera <mmosquera@makingsense.com>
2019-12-31 15:44:49 -0300

    add new FullDailyReport

---
+++ Relay.BulkSenderService/Configuration/FullDailyReportTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Relay.BulkSenderService.Classes;
+using Relay.BulkSenderService.Reports;
+
+namespace Relay.BulkSenderService.Configuration
+{
+    public class FullDailyReportTypeConfiguration : DailyReportTypeConfiguration
+    {
+        public override ReportProcessor GetReportProcessor(IConfiguration configuration, ILog logger)
+        {
+            return new FullDailyReportProcessor(logger, configuration, this);
+        }
+    }
+}
