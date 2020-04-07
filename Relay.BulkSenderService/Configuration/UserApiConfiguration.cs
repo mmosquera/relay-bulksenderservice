194715099492759bca8fd987ddd1b63fdb595d88
mmosquera <mmosquera@makingsense.com>
2020-04-07 09:28:53 -0300

    order templates by name parts

--- Relay.BulkSenderService/Configuration/UserApiConfiguration.cs
+++ Relay.BulkSenderService/Configuration/UserApiConfiguration.cs
@@ -50,7 +50,11 @@ namespace Relay.BulkSenderService.Configuration
         {
             string name = Path.GetFileNameWithoutExtension(fileName);
 
-            foreach (ITemplateConfiguration templateConfiguration in Templates.Where(x => !x.FileNameParts.Contains("*")))
+            var orderedTemplates = Templates.Where(x => !x.FileNameParts.Contains("*"))
+                .OrderByDescending(x => x.FileNameParts.Count)
+                .ThenByDescending(x => x.FileNameParts.Max(y => y.Length));
+
+            foreach (ITemplateConfiguration templateConfiguration in orderedTemplates)
             {
                 if (templateConfiguration.FileNameParts.All(x => name.Contains(x, StringComparison.InvariantCultureIgnoreCase)))
                 {
